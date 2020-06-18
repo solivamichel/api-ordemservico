20265db25529a78276392005938dc95d4a68209c
Michel Soliva <michelsolivadev@gmail.com>
2020-06-18 14:43:39 -0300

    Implementando funcionalidades e bean validation

--- src/main/java/com/soliva/osworks/domain/model/Cliente.java
+++ src/main/java/com/soliva/osworks/domain/model/Cliente.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente implements Serializable {
@@ -15,10 +18,17 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
 	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
 	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 
 	public Long getId() {
